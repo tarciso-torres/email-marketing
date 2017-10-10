20715772c139d4e7152f6b4fe9cc078a64887941
“Tarciso <tarcisoftorres@g4flex.com.br>
2017-10-09 21:59:01 -0300

    Criação do modelo user

---
+++ p/src/models/user.js
@@ -0,0 +1,17 @@
+/**
+ * Created by tarciso on 10/9/17.
+ */
+let mongoose = require('mongoose');
+
+let User = mongoose.Schema({
+    name: String,
+    email: {type: String, unique: true},
+    password: String,
+    accounts: [{
+        name: String,
+        role: String,
+        enabled: Boolean
+    }]
+});
+
+module.exports = mongoose.model('User', User);
