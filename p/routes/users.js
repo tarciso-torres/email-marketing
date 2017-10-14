04daef61d9da2665933fee321af8078398720f2a
“Tarciso <tarcisoftorres@g4flex.com.br>
2017-10-14 14:44:56 -0300

    User
    Criação da entidade user

--- p/routes/users.js
+++ p/routes/users.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let User = require('./../src/models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', function(req, res, next) {
+    let data = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      accounts: [{
+        name: req.body.account_name,
+        role: 'owner',
+        enabled: true
+      }]
+    }
+
+    let callback = function (err, user) {
+        if(err){
+          return res.status(422).json({err: err});
+        }
+        return res.status(200).json({user: user});
+    };
+
+    User.create(data, callback);
 });
 
 module.exports = router;
